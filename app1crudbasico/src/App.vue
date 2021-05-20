6509c3431234081a4618ad5c3fee3dce59ec7269
Mateus Vitorino Guimarães <mateusvitorino@gmail.com>
2021-05-20 09:05:48 -0300

    Alterações Básicas

---
+++ app1crudbasico/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Crud/>
+</template>
+
+<script>
+import Crud from './components/AppBasicoCrud'
+
+export default {
+  name: 'App',
+  components: {
+    Crud
+  }
+}
+</script>
+
+<style>
+
+</style>
